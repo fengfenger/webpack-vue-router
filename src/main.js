68daec2da4c0c88f07681311c9a2c09c817e9c28
xifeng.cao <xifeng.cao@yinyuetai.com>
2016-07-25 11:56:40 +0800

    去除删格系统

--- src/main.js
+++ src/main.js
@@ -9,9 +9,6 @@ import VueRouter from 'vue-router';
 import fastclick from 'fastclick';
 fastclick.attach(document.body);
 
-// css的重置样式 目前rem机制还没有引进来
-// import './styles/common/reset.scss';
-
 import registerRouters from './routers';
 import App from './app.vue';
 
