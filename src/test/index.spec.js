8a1a391d2783cb8cbbee24dc5a28b85dd7cba41b
xifeng.cao <xifeng.cao@yinyuetai.com>
2016-07-25 09:51:56 +0800

    test project

---
+++ src/test/index.spec.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+describe('test',function(){
+  describe('#test()',function(){
+    it('test',function(){
+        expect(1,2)
+    });
+  });
+});
